504a841e23dcd30e6abebdacb61c66fb6792f0c3
SamuelMachulak <machulak.bonfim@escola.pr.gov.br>
2025-07-29 08:00:12 -0300

    retorno

--- main.js
+++ main.js
@@ -16,4 +16,5 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
 let tempoAtual = new Date();
 
+
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
